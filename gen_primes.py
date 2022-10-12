7c228f0f1f6038b52d6112fd19a55622ea2a9642
GaryCrowe83 <gcrowe30@gmail.com>
2022-10-12 08:42:47 +0100

    Added comments to gen_primes.py.

--- gen_primes.py
+++ gen_primes.py
@@ -1,25 +1,30 @@
+"""
+Program to generate first 1000 primes to be used in other scripts in this projec.
+
+"""
+
 import math
 
+
 def main():
-	primes = []
-	count = 0
-	i = 1
-	while count <= 1000:
-		if isPrime(i):
-			primes.append(i)
-			count = count + 1
-		i = i + 1
+    primes = []
+    count = 0
+    i = 1
+    while count <= 1000:
+        if isPrime(i):
+            primes.append(i)
+            count = count + 1
+        i = i + 1
 
-	print(primes)
-
+    print(primes)
 
 
 def isPrime(n):
-	for x in range(2, n):
-		if n % x == 0:
-			return False
-	return True
+    for x in range(2, n):
+        if n % x == 0:
+            return False
+    return True
 
 
 if __name__ == "__main__":
-	main()
+    main()
